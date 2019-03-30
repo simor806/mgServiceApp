0e43788b3f47d5729eeb5e2dbcc734232f99887f
Szymon Radziej <szymon.radziej@gmail.com>
2019-03-30 21:11:44 +0100

    Feat - dodanie prowizorycznego routingu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {VehiclesComponent} from './dashboard/vehicles/vehicles.component';
+import {DiaryComponent} from './dashboard/diary/diary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: VehiclesComponent},
+  {path: 'diary', component: DiaryComponent},
+  {path: '**', redirectTo: 'dashboard'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
