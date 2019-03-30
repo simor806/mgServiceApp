340e0fe55ee6e24b40109fa28f2af404be37962a
Szymon Radziej <szymon.radziej@gmail.com>
2019-03-30 21:11:23 +0100

    Feat - dodanie komponentu Diary

---
+++ src/app/dashboard/diary/diary.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {StaticDataSource} from '../../model/static.datasource';
+import {Vehicle} from '../../model/vehicle';
+import {DiaryEntry} from '../../model/diary-entry';
+
+@Component({
+  selector: 'app-diary',
+  templateUrl: './diary.component.html',
+  styleUrls: ['./diary.component.sass']
+})
+export class DiaryComponent implements OnInit {
+
+  public diary: DiaryEntry[];
+
+  constructor(private staticDS: StaticDataSource) { }
+
+  ngOnInit() {
+    this.staticDS.getDiary(1).subscribe((diary: DiaryEntry[]) => this.diary = diary);
+  }
+
+}
