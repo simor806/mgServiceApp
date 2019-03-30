41408c31b50b68e4090f910f33688519196807b7
Szymon Radziej <szymon.radziej@gmail.com>
2019-03-30 21:04:20 +0100

    Feat - dodanie modułu Dashboard wraz z prowizorycznym komponentem Vehicles

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import {ModelModule} from '../model/model.module';
 import { DiaryComponent } from './diary/diary.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [VehiclesComponent, DiaryComponent],
   exports: [DiaryComponent, VehiclesComponent],
   imports: [
     CommonModule,
-    ModelModule
+    ModelModule,
+    RouterModule
   ]
 })
 export class DashboardModule { }
