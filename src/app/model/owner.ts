296e7cc4772f4ac3c6784374823a571ca80d7dac
Szymon Radziej <szymon.radziej@gmail.com>
2019-04-18 21:53:51 +0200

    Feat - rozszerzenie klasy Owner o pole fullName.

--- src/app/model/owner.ts
+++ src/app/model/owner.ts
@@ -10,15 +10,16 @@ export class Owner {
   id: number;
   firstName: string;
   lastName: string;
+  fullName: string;
   phone: string;
   note: string;
 
-  constructor(
-    ownerAttrs: Partial<OwnerAttrs> = {}) {
+  constructor(ownerAttrs: Partial<OwnerAttrs> = {}) {
     this.id = ownerAttrs.id;
     this.firstName = ownerAttrs.firstName;
     this.lastName = ownerAttrs.lastName;
     this.phone = ownerAttrs.phone;
     this.note = ownerAttrs.note;
+    this.fullName = ownerAttrs.lastName + ' ' + ownerAttrs.firstName;
   }
 }
