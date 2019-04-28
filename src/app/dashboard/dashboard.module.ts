b23456abaedf8310f2fa3f45d448afdde4cda8fa
Szymon Radziej <szymon.radziej@gmail.com>
2019-04-28 19:25:28 +0200

    Feat - dodanie powiadomień o wymaganej zmianie oleju.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import {CommonModule, DatePipe} from '@angular/common';
 import { VehiclesListComponent } from './vehicles/vehicle-list/vehicles-list.component';
 import {ModelModule} from '../model/model.module';
-import { DiaryComponent } from './diary/diary.component';
 import {RouterModule} from '@angular/router';
 import {VehicleFormComponent} from './vehicles/vehicle-form/vehicle-form.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { VehicleDetailsComponent } from './vehicles/vehicle-details/vehicle-details.component';
 import {DiaryModule} from './diary/diary.module';
+import { VehicleAlertComponent } from './vehicles/vehicle-alert/vehicle-alert.component';
 
 @NgModule({
-  declarations: [VehiclesListComponent, VehicleFormComponent, VehicleDetailsComponent],
+  declarations: [VehiclesListComponent, VehicleFormComponent, VehicleDetailsComponent, VehicleAlertComponent],
   exports: [VehiclesListComponent],
   imports: [
     CommonModule,
---
+++ src/app/dashboard/vehicles/vehicle-alert/vehicle-alert.component.ts
@@ -0,0 +1,61 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Vehicle} from '../../../model/vehicle';
+import {DiaryEntry} from '../../../model/diary-entry';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-vehicle-alert',
+  templateUrl: './vehicle-alert.component.html',
+  styleUrls: ['./vehicle-alert.component.scss']
+})
+export class VehicleAlertComponent implements OnInit {
+
+  private readonly DAYS_IN_YEAR = 365;
+  private readonly YEARS_TO_OIL_CHANGE_FOR_LONGLIFE = 2;
+  public readonly MILEAGE_TO_OIL_CHANGE = 10000;
+  public readonly MILEAGE_TO_OIL_LONGLIFE_CHANGE = 30000;
+  @Input() public vehicle: Vehicle;
+  public shouldChangeOilBecauseDate = false;
+  public shouldChangeOilBecauseMileage = false;
+  public oilNeverChanged = false;
+  public diffBetweenLastRepairAndLastOilChange: number;
+  public diffBetweenMileages: number;
+  public timeToOilChange = this.DAYS_IN_YEAR;
+  public timeToOilLonglifeChange = this.DAYS_IN_YEAR * this.YEARS_TO_OIL_CHANGE_FOR_LONGLIFE;
+
+  constructor() { }
+
+  ngOnInit() {
+    if (this.vehicle && this.vehicle.diary.length > 0) {
+      const diaryEntriesWithLastOilChanged = this.vehicle.diary.filter((diaryEntry: DiaryEntry) => diaryEntry.isOilChanged);
+      if (diaryEntriesWithLastOilChanged.length > 0) {
+        this.checkOilByDate(diaryEntriesWithLastOilChanged[0]);
+        this.checkOilByMileage(diaryEntriesWithLastOilChanged[0]);
+      }
+    } else {
+      this.oilNeverChanged = true;
+    }
+  }
+
+  private checkOilByDate(diaryEntryWithLastOilChanged: DiaryEntry) {
+    const lastRepairDate = moment(this.vehicle.diary[0].date);
+    const lastOilChangeDate = moment(diaryEntryWithLastOilChanged.date);
+    this.diffBetweenLastRepairAndLastOilChange = lastRepairDate.diff(lastOilChangeDate, 'days');
+    if (this.vehicle.hasLongLifeOil) {
+      this.shouldChangeOilBecauseDate = this.diffBetweenLastRepairAndLastOilChange > this.timeToOilLonglifeChange;
+    } else {
+      this.shouldChangeOilBecauseDate = this.diffBetweenLastRepairAndLastOilChange > this.timeToOilChange;
+    }
+  }
+
+  private checkOilByMileage(diaryEntryWithLastOilChanged: DiaryEntry) {
+    const lastRepairMileage = this.vehicle.diary[0].mileage;
+    const lastOilChangeMileage = diaryEntryWithLastOilChanged.mileage;
+    this.diffBetweenMileages = lastRepairMileage - lastOilChangeMileage;
+    if (this.vehicle.hasLongLifeOil) {
+      this.shouldChangeOilBecauseMileage = this.diffBetweenMileages > this.MILEAGE_TO_OIL_LONGLIFE_CHANGE;
+    } else {
+      this.shouldChangeOilBecauseMileage = this.diffBetweenMileages > this.MILEAGE_TO_OIL_CHANGE;
+    }
+  }
+}
