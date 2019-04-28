b7d7f8d0e9fbcb25659bff7be02d4b87cd348481
Szymon Radziej <szymon.radziej@gmail.com>
2019-04-28 16:40:57 +0200

    Fix - sortowanie wpisów dziennika po dacie.

--- src/app/dashboard/vehicles/vehicle-details/vehicle-details.component.ts
+++ src/app/dashboard/vehicles/vehicle-details/vehicle-details.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Vehicle} from '../../../model/vehicle';
 import {VehicleService} from '../vehicle.service';
 import {ActivatedRoute} from '@angular/router';
+import {DiaryEntry} from '../../../model/diary-entry';
 
 @Component({
   selector: 'app-vehicle-details',
@@ -13,14 +14,26 @@ export class VehicleDetailsComponent implements OnInit {
   public vehicle: Vehicle;
   public vehicleId: number;
 
-  constructor(private route: ActivatedRoute, private vehicleService: VehicleService) { }
+  constructor(private route: ActivatedRoute, private vehicleService: VehicleService) {
+  }
 
   ngOnInit() {
     this.vehicleId = Number(this.route.snapshot.params.vehicleId);
-    this.vehicleService.getVehicle(this.vehicleId).subscribe((vehicle: Vehicle) => this.vehicle = vehicle);
+    this.vehicleService.getVehicle(this.vehicleId).subscribe((vehicle: Vehicle) => {
+      this.vehicle = vehicle;
+      this.sortDiaryByDate(this.vehicle.diary);
+    });
   }
 
   public deleteVehicle(vehicleId: number) {
     this.vehicleService.deleteVehicle(vehicleId);
+  }
+
+  private sortDiaryByDate(diary: DiaryEntry[]) {
+    diary.sort((previousDiaryEntry, nextDiaryEntry) => {
+      const a = previousDiaryEntry.date.split('/').reverse().join();
+      const b = nextDiaryEntry.date.split('/').reverse().join();
+      return b < a ? -1 : (b > a ? 1 : 0);
+    });
   }
 }
