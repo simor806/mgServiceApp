0924f189afd6b8023b2c5b96b1d7483854a44d90
Szymon Radziej <szymon.radziej@gmail.com>
2020-04-10 22:08:32 +0200

    Fix - drobne poprawki na liście klientów.

--- src/app/dashboard/owners/owners-list/owners-list.component.ts
+++ src/app/dashboard/owners/owners-list/owners-list.component.ts
@@ -21,11 +21,4 @@ export class OwnersListComponent implements OnInit {
     this.ownerService.getOwners().subscribe((owners: Owner[]) => this.dataSource = new MatTableDataSource(owners));
   }
 
-  // TODO: move to client-details component
-  public deleteOwner(ownerId: number) {
-    this.ownerService.deleteOwner(ownerId).subscribe((owner: Owner) => {
-      this.owners.splice(this.owners.findIndex((ownerItem: Owner) => ownerItem.id === ownerId), 1);
-    }, () => console.log(`Error deleting client with id=${ownerId}`));
-  }
-
 }
