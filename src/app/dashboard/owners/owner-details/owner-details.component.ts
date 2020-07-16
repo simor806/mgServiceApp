bf6bbb02fef9f9c9bfd063cf5baa6398529b4af1
Szymon Radziej <szymon.radziej@gmail.com>
2020-07-16 23:21:11 +0200

    Feat - Usuwanie/aktualizacja pojazdów przy usuwaniu klientów.

--- src/app/dashboard/owners/owner-details/owner-details.component.ts
+++ src/app/dashboard/owners/owner-details/owner-details.component.ts
@@ -5,6 +5,8 @@ import {map} from 'rxjs/operators';
 import {ConfirmationDialogComponent} from '../../gui/shared-components/dialogs/confirmation-dialog/confirmation-dialog.component';
 import {MatDialog, MatDialogRef} from '@angular/material';
 import {OwnerService} from '../owner.service';
+import {VehicleService} from '../../vehicles/vehicle.service';
+import {Vehicle, VehicleAttrs} from '../../../model/vehicle';
 
 @Component({
   selector: 'app-owner-details',
@@ -17,7 +19,12 @@ export class OwnerDetailsComponent implements OnInit {
 
   private dialogRef: MatDialogRef<ConfirmationDialogComponent>;
 
-  constructor(private dialog: MatDialog, private ownerService: OwnerService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private dialog: MatDialog,
+              private ownerService: OwnerService,
+              private vehicleService: VehicleService,
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit() {
     this.route.data
@@ -25,17 +32,37 @@ export class OwnerDetailsComponent implements OnInit {
       .subscribe((owner: Owner) => this.owner = owner);
   }
 
-  openDeleteOwnerConfirmationDialog() {
+  public openDeleteDialog() {
+    this.vehicleService.getVehicles().subscribe((vehicles: Vehicle[]) => {
+      const vehiclesIdToDelete: number[] = [];
+      const vehiclesToUpdate: Vehicle[] = [];
+      vehicles.forEach((vehicle: Vehicle) => {
+        if (vehicle.ownersIds.length === 1 && vehicle.ownersIds.includes(this.owner.id)) {
+          vehiclesIdToDelete.push(vehicle.id);
+        }
+        if (vehicle.ownersIds.length > 1 && vehicle.ownersIds.includes(this.owner.id)) {
+          vehiclesToUpdate.push(vehicle);
+        }
+      });
+      this.openDeleteOwnerAndVehiclesConfirmationDialog(vehiclesIdToDelete, vehiclesToUpdate);
+    });
+  }
+
+  private openDeleteOwnerAndVehiclesConfirmationDialog(vehiclesIdToDelete: number[], vehiclesToUpdate: Vehicle[]) {
     this.dialogRef = this.dialog.open(ConfirmationDialogComponent, {
       disableClose: false
     });
-    this.dialogRef.componentInstance.confirmTitle = 'Usunąć klienta?';
-    this.dialogRef.componentInstance.confirmMessage = `Klient ${this.owner.fullName} zostanie trwale usunięty z bazy.`;
+    this.dialogRef.componentInstance.confirmTitle = vehiclesIdToDelete.length ? 'Usunąć klienta wraz z pojazdami?' : 'Usunąć klienta?';
+    this.dialogRef.componentInstance.confirmMessage = vehiclesIdToDelete.length
+      ? `Klient ${this.owner.fullName} posiada przypisane do niego pojazdy. Klient wraz z przypisanymi do niego pojazdami zostanie trwale usunięty z bazy.`
+      : `Klient ${this.owner.fullName} zostanie trwale usunięty z bazy.`;
     this.dialogRef.componentInstance.confirmColor = 'warn';
     this.dialogRef.componentInstance.confirmButton = 'Usuń';
     this.dialogRef.afterClosed().subscribe(result => {
       if (result) {
         this.deleteOwner();
+        this.deleteVehicles(vehiclesIdToDelete);
+        this.updateVehicles(vehiclesToUpdate);
       }
       this.dialogRef = null;
     });
@@ -45,5 +72,27 @@ export class OwnerDetailsComponent implements OnInit {
     this.ownerService.deleteOwner(this.owner.id).subscribe(
       () => this.router.navigate(['/owners']),
       () => console.log('Wystąpił błąd podczas usuwania klienta'));
+  }
+
+  private deleteVehicles(vehiclesIdToDelete: number[]) {
+    vehiclesIdToDelete.forEach((vehicleId: number) => this.vehicleService.deleteVehicle(vehicleId).subscribe(
+      () => {},
+      () => console.log('Wystąpił błąd podczas usuwania pojazdów')
+    ));
+  }
+
+  private updateVehicles(vehiclesToUpdate: Vehicle[]) {
+    vehiclesToUpdate.forEach((vehicle: Vehicle) => {
+      const index = vehicle.ownersIds.indexOf(this.owner.id);
+      vehicle.ownersIds.splice(index, 1);
+      delete vehicle.diary;
+      const vehicleAttrs = {
+        ...vehicle
+      } as VehicleAttrs;
+      this.vehicleService.saveVehicle(vehicleAttrs).subscribe(
+        () => {},
+        () => console.log('Wystąpił błąd podczas aktualizacji pojazdów')
+      );
+    });
   }
 }
