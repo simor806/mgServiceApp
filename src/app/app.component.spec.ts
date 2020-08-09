eeefa129429cb6f8ba41fa64c228fccd8f7b7288
Szymon Radziej <szymon.radziej@gmail.com>
2020-08-09 19:33:24 +0200

    Fix - Poprawienie nazwy aplikacji.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MgService'`, () => {
+  it(`should have as title 'MGservice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MgService');
+    expect(app.title).toEqual('MGservice');
   });
 
   it('should render title in a h1 tag', () => {
