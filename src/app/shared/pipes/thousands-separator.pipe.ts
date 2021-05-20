df3eebd1398696c34196f32b23d6e43cfe3c28c2
Szymon Radziej <szymon.radziej@gmail.com>
2021-05-20 20:10:02 +0200

    Fix - Poprawienie działania pipe'a separatora tysięcznego.

--- src/app/shared/pipes/thousands-separator.pipe.ts
+++ src/app/shared/pipes/thousands-separator.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ThousandsSeparatorPipe implements PipeTransform {
 
   transform(value: string | number): string {
-    if (value === undefined || value === null) {
-      return null;
+    if (value === undefined || value === null || value === '') {
+      return '?';
     }
     return parseInt(String(value), 10).toLocaleString();
   }
